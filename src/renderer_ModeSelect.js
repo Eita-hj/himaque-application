a81209125fc510f711db7ae2600ce339d6173874
noir-chat-9661 <eita9661@gmail.com>
2025-05-08 20:03:06 +0900

    Update: Refactor send and ready functions in renderer_ModeSelect.js for improved readability and structure

--- src/renderer_ModeSelect.js
+++ src/renderer_ModeSelect.js
@@ -4,13 +4,17 @@ function send() {
 		[...document.getElementsByName("addon")].find((n) => n.checked).value ==
 		"true";
 	obj.windowCount = document.getElementById("window").value;
+
 	if (["2", "3", "4"].includes(obj.windowCount))
 		obj.type = document.getElementById("windowtype").value;
+	obj.mode = [...document.getElementsByName("mode")].find(
+		(n) => n.checked
+	).value;
 	obj.addonModules = {
 		multilinechat: document.getElementById("multilinechataddon").checked,
 		chatmaxup: document.getElementById("chatmaxupaddon").checked,
-		displaystatus: document.getElementById("displaystatusaddon").checked
-	}
+		displaystatus: document.getElementById("displaystatusaddon").checked,
+	};
 	window.electronAPI.start(obj);
 }
 
@@ -21,9 +25,20 @@ function ready() {
 	[...document.getElementsByName("addon")].find(
 		(m) => m.value == `${n.addon}`
 	).checked = true;
-	if (["2", "3", "4"].includes(n.windowCount)) document.getElementById("windowtype").value = n.type;
-	document.getElementById('addonType').style.display = n.addon ? "" : "none";
-	document.getElementById("multilinechataddon").checked = !!n.addonModules?.multilinechat;
-	document.getElementById("chatmaxupaddon").checked = !!n.addonModules?.chatmaxup;
-	document.getElementById("displaystatusaddon").checked = !!n.addonModules?.displaystatus;
+	if (["2", "3", "4"].includes(n.windowCount))
+		document.getElementById("windowtype").value = n.type;
+	document.getElementById("addonType").style.display = n.addon ? "" : "none";
+	document.getElementById("multilinechataddon").checked =
+		!!n.addonModules?.multilinechat;
+	document.getElementById("chatmaxupaddon").checked =
+		!!n.addonModules?.chatmaxup;
+	document.getElementById("displaystatusaddon").checked =
+		!!n.addonModules?.displaystatus;
+	if (n.mode == "window")
+		document.getElementById("windowcount").style.display = "";
+	(
+		[...document.getElementsByName("mode")].find(
+			(m) => m.value == n.mode
+		) || document.getElementsByName("mode")[0]
+	).checked = true;
 }
