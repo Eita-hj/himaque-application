af7c69f8396a35835d11dbd97eaf4951b08448b4
noir-chat-9661 <eita9661@gmail.com>
2025-05-11 01:30:15 +0900

    Fix: Implement tab management functionality and enhance event handling for improved user interaction

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,286 @@
 window.addEventListener("DOMContentLoaded", async () => {
-	if (!new URL(location.href).origin.includes("himaquest.com")) return;
-
 	const { ipcRenderer } = require("electron");
+	const hcqLinks = [
+		{
+			url: "https://himaquest.com",
+			used: false,
+			id: 0,
+		},
+		{
+			url: "http://himaquest.com.",
+			used: false,
+			id: 1,
+		},
+		{
+			url: "http://www.himaquest.com",
+			used: false,
+			id: 2,
+		},
+		{
+			url: "http://www.himaquest.com.",
+			used: false,
+			id: 3,
+		},
+		{
+			url: "http://sub1.himaquest.com",
+			used: false,
+			id: 4,
+		},
+		{
+			url: "http://sub1.himaquest.com.",
+			used: false,
+			id: 5,
+		},
+		{
+			url: "http://sub2.himaquest.com",
+			used: false,
+			id: 6,
+		},
+		{
+			url: "http://sub2.himaquest.com.",
+			used: false,
+			id: 7,
+		},
+		{
+			url: "http://sub3.himaquest.com",
+			used: false,
+			id: 8,
+		},
+		{
+			url: "http://sub3.himaquest.com.",
+			used: false,
+			id: 9,
+		},
+	];
+	if (window.self === window.top) {
+		const d = ipcRenderer.sendSync("startgame");
+		if (d.mode == "tab") {
+			const $ = window.jQuery;
+			const f = {};
+
+			f.tabClose = (e) => {
+				const t = $(e.target).closest(".tab");
+				if (t.hasClass("active")) {
+					const next = t.next(".tab").length
+						? t.next(".tab")
+						: t.prev(".tab");
+					if (next.length) {
+						f.tabChange({ target: next });
+					}
+				}
+				const id = Number(t.attr("name"));
+				hcqLinks.find((n) => n.id == id).used = false;
+				$(e.target).closest(".tab").remove();
+				$(`iframe[name="${id}"]`).remove();
+				if (!hcqLinks.find((n) => n.used)) f.tabAdd();
+			};
+			f.tabChange = (e) => {
+				const id = Number($(e.target).closest(".tab").attr("name"));
+				$(".tab").removeClass("active");
+				$(e.target).closest(".tab").addClass("active");
+				$("iframe").hide();
+				$(`iframe[name="${id}"]`).show();
+				document.title = `表示中のタブ：タブ${id + 1}`;
+			};
+			f.tabAdd = () => {
+				const d = hcqLinks.find((n) => !n.used);
+				if (!d) {
+					alert("タブの上限に達しました。");
+					return;
+				}
+				d.used = true;
+				$("#tabs").append(
+					`<div class="tab" name="${d.id}"><p>タブ${
+						d.id + 1
+					}<span class="close">×</span></p></div>`
+				);
+				$(`.tab[name="${d.id}"] p`).on("click", (e) => f.tabChange(e));
+				$(`.tab[name="${d.id}"] .close`).on("click", (e) => f.tabClose(e));
+				$("#gamearea").append(
+					`<iframe src="${d.url}" name="${d.id}"></iframe>`
+				);
+				const h = $(`iframe[name="${d.id}"]`);
+				f.tabChange({ target: $(`.tab[name="${d.id}"]`) });
+			};
+			$("#tabaddbtn")
+				.removeAttr("id")
+				.on("click", () => f.tabAdd());
+
+			$("#tabarea").on("wheel", (e) => {
+				if (Math.abs(e.originalEvent.deltaY) < Math.abs(e.originalEvent.deltaX))
+					return;
+
+				const maxScrollLeft =
+					$("#tabs").get(0).scrollWidth - $("#tabs").get(0).clientWidth;
+
+				if (
+					($("#tabs").scrollLeft() <= 0 && e.originalEvent.deltaY < 0) ||
+					($("#tabs").scrollLeft() >= maxScrollLeft &&
+						e.originalEvent.deltaY > 0)
+				)
+					return;
+
+				e.preventDefault();
+				$("#tabs").scrollLeft($("#tabs").scrollLeft() + e.originalEvent.deltaY);
+			});
+
+			f.keydownEvent = (e) => {
+				if (e.ctrlKey && e.key === "Tab") {
+					e.preventDefault();
+					const activeTab = $(".tab.active");
+					const nextTab = activeTab.next(".tab").length
+						? activeTab.next(".tab")
+						: $(".tab").first();
+					const prevTab = activeTab.prev(".tab").length
+						? activeTab.prev(".tab")
+						: $(".tab").last();
+					return f.tabChange({ target: e.shiftKey ? prevTab : nextTab });
+				}
+				if (e.ctrlKey && e.key === "w") {
+					e.preventDefault();
+					const activeTab = $(".tab.active");
+					return f.tabClose({ target: activeTab });
+				}
+				if (e.ctrlKey && e.key === "t") {
+					e.preventDefault();
+					return f.tabAdd();
+				}
+				if ((e.ctrlKey && e.key === "r") || e.key === "F5") {
+					e.preventDefault();
+					const activeTab = $(".tab.active");
+					const id = Number(activeTab.attr("name"));
+					$(`iframe[name="${id}"]`)
+						.removeAttr("src")
+						.attr("src", hcqLinks.find((n) => n.id == id).url);
+					return;
+				}
+				if ((e.ctrlKey && e.key === "s") || e.key === "F1") {
+					e.preventDefault();
+					ipcRenderer.send("state", {
+						type: "partyReady",
+					});
+					return;
+				}
+				if ((e.ctrlKey && e.key === "b") || e.key === "F2") {
+					e.preventDefault();
+					ipcRenderer.send("state", {
+						type: "exitField",
+					});
+					return;
+				}
+			};
+			f.tabAdd();
+
+			setInterval(() => {
+				ipcRenderer.invoke("events").then((s) => {
+					if (s.add) f.tabAdd();
+					if (s.close) f.tabClose({ target: $(`.tab.active`) });
+					if (s.change) {
+						const activeTab = $(".tab.active");
+						const nextTab = activeTab.next(".tab").length
+							? activeTab.next(".tab")
+							: $(".tab").first();
+						f.tabChange({ target: nextTab });
+					}
+					if (s.change2) {
+						const activeTab = $(".tab.active");
+						const prevTab = activeTab.prev(".tab").length
+							? activeTab.prev(".tab")
+							: $(".tab").last();
+						f.tabChange({ target: prevTab });
+					}
+					if (s.reload.length) {
+						s.reload.forEach((url) => {
+							const id = hcqLinks.find((n) => n.url == url).id;
+							$(`iframe[name="${id}"]`)
+								.removeAttr("src")
+								.attr("src", url);
+						});
+					}
+				});
+			}, 200);
+		} else if (d.mode == "window") {
+			const $ = window.jQuery;
+			const DOM = hcqLinks.slice(0, Number(d.windowCount)).map(n => `<iframe src="${n.url}"></iframe>`)
+			$("#gamearea").html(DOM);
+			switch (Number(d.windowCount)) {
+				case 1:
+					$("iframe").css({
+						width: "calc(100% - 5px)",
+						height: "calc(100% - 10px)",
+						border: "none",
+					});
+					break;
+				case 2:
+					$("iframe").css({
+						width: d.type == "a" ? "calc(100% - 5px)" : "calc(50% - 5px)",
+						height: d.type == "a" ? "calc(50% - 10px)" : "calc(100% - 10px)",
+						border: "none",
+					});
+					break;
+				case 3:
+					$("iframe").css({
+						width: d.type == "a" ? "calc(100% - 5px)" :"calc(33% - 5px)",
+						height: d.type == "a" ? "calc(33% - 10px)" : "calc(100% - 10px)",
+						border: "none",
+					});
+					break;
+				case 4:
+					$("iframe").css({
+						width: d.type == "a" ? "calc(50% - 5px)" : "calc(25% - 5px)",
+						height: d.type == "a" ? "calc(50% - 10px)" : "calc(100% - 10px)",
+						border: "none",
+					});
+					break;
+				case 5:
+					$("iframe").css({
+						width: "calc(1/3 - 5px)",
+						height: "calc(50% - 10px)",
+						border: "none",
+					});
+					$("iframe:eq(0)").css("height", "calc(100% - 10px)");
+					break;
+				case 6:
+					$("iframe").css({
+						width: "calc(1/3 - 5px)",
+						height: "calc(50% - 10px)",
+						border: "none",
+					});
+					break;
+				case 7:
+					$("iframe").css({
+						width: "calc(1/3 - 5px)",
+						height: "calc(50% - 10px)",
+						border: "none",
+					});
+					$("iframe:eq(0)").css("height", "calc(100% - 10px)");
+					break;
+				case 8:
+					$("iframe").css({
+						width: "calc(25% - 5px)",
+						height: "calc(50% - 10px)",
+						border: "none",
+					});
+					break;
+				case 9:
+					$("iframe").css({
+						width: "calc(1/3 - 5px)",
+						height: "calc(1/3 - 10px)",
+						border: "none",
+					});
+					break;
+				case 10:
+					$("iframe").css({
+						width: "calc(20% - 5px)",
+						height: "calc(50% - 10px)",
+						border: "none",
+					});
+					break;
+			}
+		}
+		return;
+	}
+	if (!new URL(location.href).origin.includes("himaquest.com")) return;
 	document.body.style.display = "none";
 	let _tmp = {};
 	_tmp = setTimeout(() => location.reload(), 1500);
@@ -94,11 +373,6 @@ window.addEventListener("DOMContentLoaded", async () => {
 		};
 
 		const { addonModules } = ipcRenderer.sendSync("startgame");
-		
-		ipcRenderer.on("exitfield", () => {
-			console.log("called")
-			ExitField()
-		});
 
 		if (addonModules.multilinechat)
 			await fetch("https://addon.pjeita.top/module/multilinechat.js", {
@@ -239,9 +513,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 		}
 		if (e.ctrlKey && e.key === "w") {
 			e.preventDefault();
-			ipcRenderer.send("tabClose", {
-				url: new URL(location.href).origin,
-			});
+			ipcRenderer.send("tabClose");
 			return;
 		}
 		if (e.ctrlKey && e.key === "t") {
@@ -258,12 +530,16 @@ window.addEventListener("DOMContentLoaded", async () => {
 		}
 		if ((e.ctrlKey && e.key === "s") || e.key === "F1") {
 			e.preventDefault();
-			ipcRenderer.send("partyReady")
+			ipcRenderer.send("state", {
+				type: "partyReady"
+			})
 			return;
 		}
 		if ((e.ctrlKey && e.key === "b") || e.key === "F2") {
 			e.preventDefault();
-			ipcRenderer.send("exitField")
+			ipcRenderer.send("state", {
+				type: "exitField"
+			})
 			return;
 		}
 	});
@@ -274,14 +550,19 @@ window.addEventListener("DOMContentLoaded", async () => {
 		}).then((s) => {
 			if (!s) return;
 			if (s.partyReady) {
-				if (myparty) {
-					PartyQuestReady();
+				if (myparty && now_scene == 20) {
+					setTimeout(PartyQuestReady, hcqLinks.find(n => n.url == new URL(location.href).origin).id * 1000);
 				}
 			}
 			if (s.exitField) {
+				if (now_channel) ExitField();
 				if (now_scene != 20) {
-					ExitField();
-					setTimeout(() => PorchResultComplete(0), 500);
+					setTimeout(() => {
+						if (!$("#zisatudiv").length) {
+							if (now_scene == 31) ExitQuest();
+							setTimeout(() => PorchResultComplete(0), 500);
+						}
+					}, 500);
 				}
 			}
 		});
