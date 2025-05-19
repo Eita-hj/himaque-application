28366b1b6a1cc9fecf6f5de6b6d3b41ef693d72c
noir-chat-9661 <eita9661@gmail.com>
2025-05-19 23:01:01 +0900

    Fix: Add event listener for "test" message in preload_ModeSelect.js

--- src/preload_ModeSelect.js
+++ src/preload_ModeSelect.js
@@ -1,5 +1,7 @@
 const { contextBridge, ipcRenderer } = require("electron/renderer");
 
+ipcRenderer.on("test", console.log)
+
 contextBridge.exposeInMainWorld("electronAPI", {
 	start: (obj) => ipcRenderer.send("start", obj),
 	ready: () => ipcRenderer.sendSync("ready"),
