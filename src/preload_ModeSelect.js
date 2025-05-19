18cda71c78dd79df1a89510cb1cdf8ba8a3c3e00
noir-chat-9661 <eita9661@gmail.com>
2025-05-20 00:04:50 +0900

    Fix: Remove unused event listener for "test" message in preload_ModeSelect.js

--- src/preload_ModeSelect.js
+++ src/preload_ModeSelect.js
@@ -1,7 +1,5 @@
 const { contextBridge, ipcRenderer } = require("electron/renderer");
 
-ipcRenderer.on("test", console.log)
-
 contextBridge.exposeInMainWorld("electronAPI", {
 	start: (obj) => ipcRenderer.send("start", obj),
 	ready: () => ipcRenderer.sendSync("ready"),
